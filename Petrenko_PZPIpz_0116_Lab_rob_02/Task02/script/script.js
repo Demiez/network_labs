f8fca2aadaa447a4fc4a3f367a4e3f129371b77a
Dmytro Petrenko <dmytro.petrenko@sigma.software>
2020-05-09 13:44:52 +0300

    table, highlight update

--- Petrenko_PZPIpz_0116_Lab_rob_02/Task02/script/script.js
+++ Petrenko_PZPIpz_0116_Lab_rob_02/Task02/script/script.js
@@ -1,4 +1,3 @@
-// Создадим таблицу с рядами
 const createMultiplyTable = (x,y) => {
     let table = document.createElement("table");
     document.body.appendChild(table);
@@ -7,43 +6,25 @@ const createMultiplyTable = (x,y) => {
         table.appendChild(tr);
         for (let j = 0; j < y; j++) {
             let td = document.createElement("td");
-            if (i == 0) {
-                td.innerText = `${j}`;
-            }
-            if (j == 0 && i != 0) {
-                td.innerText = `${i}`;
-            }
-            if (j != 0 && i != 0){
-                td.innerText = `${i*j}`;
-            }
             tr.appendChild(td);
         }
     }
 }
 
-const highlightTd = event => {
-    let td = event.target; //В объекте событий в таргете лежит нужный нам td
-    let tr = td.parentElement.children; // В tr заносим всех детей на уровне нашего td и формируем массив
-    let tb = td.parentElement.parentElement.children; // В tb - дети table (все tr)
-
-    for (let key of tr) { //здесь key - остальные td в ряду с нашим, если cellIndex одинаковый - подсвечиваем ячейку (td в target), иначе - зеленым всю оставшуюся строку(ряд)
-        key.cellIndex == td.cellIndex ? key.style["backgroundColor"] = "grey" : key.style["backgroundColor"] = "green";
-    }
-    for (let row of tb) { // сначала итерируем через каждый tr в table
-        if (row != td.parentElement) { // если ряд не равен ряду в таргетным td (его обработали в предыдущем for)
-            for (let data of row.children) { // итерируем через td в ряду
-                if (data.cellIndex == td.cellIndex) data.style["backgroundColor"] = "green"; //если cellIndex совпадает с таргетным td -> подсвечиваем его, таким образом формируя подсветку колонки
-            }
-        }
+const highlight = event => {
+    let td = event.target;
+    let tr = td.parentElement.children;
+    for (let key of tr) {
+        key.style["backgroundColor"] = "green";
     }
 }
 
-const clearTd = event => {
+const clear = event => {
     let td = event.target;
     let tb = td.parentElement.parentElement.children;
     for (let row of tb) {
         for (let data of row.children) {
-            data.style["backgroundColor"] = "white"; // возвращаем всем td во всех рядах белый бэкграунд
+            data.style["backgroundColor"] = "white";
         }
     }
 }
@@ -51,9 +32,9 @@ const clearTd = event => {
 createMultiplyTable(5,5);
 
 window.onload = () => {
-    let tds = document.querySelectorAll("td"); // На онлоаде получаем все td нашей таблицы в виде nodelist (коллекции узлов)
-    for (let i = 0; i < tds.length; i++) { // итерируем через них и добавляем 2 ивента (mouseover и mouseout)
-        tds[i].addEventListener("mouseover", highlightTd);
-        tds[i].addEventListener("mouseout", clearTd);
+    let tds = document.querySelectorAll("td");
+    for (let i = 0; i < tds.length; i++) {
+        tds[i].addEventListener("mouseover", highlight);
+        tds[i].addEventListener("mouseout", clear);
     }
 };
--- README.md
+++ README.md
@@ -7,23 +7,8 @@
 ### Лабораторна робота N2:
 1. Створити форму, яка містить інформацію по реєстрації користувача 
 на сайті бібліотеки. У разі правильного заповнення всіх полів видати зведену 
-введену інформацію в новому вікні. 
-1) create form constructor
-2) all layout (except container) is planned to be created with Javascript
-3) an object provided to constructor can contain different inputs (name, surname, boolean data(married, has children and so on)) and buttons (with callbacks for actions)
-4) while iterating through the object validate type of fields and buttons
-5) each input must correspond to the aim of the field (type="text" for simple fields, radio - for boolean values)
-6) user's input must be validated and if valid, data should be changed in runtime
-7) for now 2 buttons:<br>
-ok - updates data<br>
-cancel - must clear all the inputs<br>
-8) mandatory fields marked with red * (had to mutate object to avoid * in property and to save all validation checks)
-9) mandatory fields are highlighted if empty
-10) password and password confirmation validation
-11) ok button checks following before sending request (for now just into console without JSON formation):
-- if mandatory fields are not empty
-- all validators are passed
-12) cancel button restores all input fields to the moment of object creation (including radio buttons) 
+введену інформацію в новому вікні (При обробці форм перевірити правильність заповнення полів) (Варіант 1).
+2. Рухомий рядок (Варіант 1).
+3. Простий графічний редактор для малювання примітивів (лінія, коло, прямокутник) (Варіант 14).
 
-2. Рухомий рядок
-3. Простий графічний редактор для малювання примітивів (лінія, коло, прямокутник)
+### Лабораторна робота N3:
